81e79a4e7a37475768519a3abaf9494da37508dd
Daniel Gysi <danielgysi@protonmail.com>
2022-02-14 16:01:44 -0500

    implemented all instructions except display and keyboard functions

--- emu8/emu8.py
+++ emu8/emu8.py
@@ -1,3 +1,5 @@
+import random;
+
 class chip:
     def __init__():
 
@@ -151,7 +153,98 @@ class chip:
     def JP0(self, addr):
         """instruction to jump to the instruction at addr + the value
         in register 0"""
-        self.pc = addr + self.regs[0]
+        self.pc = addr + self.regs[0];
+
+    def RND(self, reg, b):
+        """instruction to generate a random byte, bitwise and it with a given
+        byte and store the result"""
+        r = random.randint(0,255);
+        self.regs[reg] = r & b;
+        self.pc += 1;
+
+    def DRW(self):
+        """instruction to draw a sprite on the display"""
+        pass;
+
+    def SKP(self, reg):
+        """instruction to skip the next instruction if the key corresponding
+        to the value in a given register is pressed"""
+        pass;
+
+    def SKNP(self, reg):
+        """instruction to skip the next instruction if the key corresponding
+        to the value in a given register is not pressed"""
+        pass;
+
+    def LDregdt(self, reg):
+        """instruction to load the value in the delay timer into a 
+        register"""
+        self.regs[reg] = self.dt;
+        self.pc += 1;
+
+    def LDkey(self, reg):
+        """instruction to load the value of the next key press into a
+        register"""
+        pass;
+
+    def LDdt(self, reg):
+        """instruction to load the value from a register into the delay
+        timer"""
+        self.dt = self.regs[reg];
+        self.pc += 1;
+
+    def LDst(self, reg):
+        """instruction to load the value from a register into the sound
+        timer"""
+        self.st = self.regs[reg];
+        self.pc += 1;
+
+    def ADDi(self, reg):
+        """instruction to add the value in a register to the I register"""
+        self.regI += self.regs[reg];
+        self.regI = self.regI & (2**12-1) #maintain 12 bits
+        self.pc += 1;
+
+    def LDdigit(self, reg):
+        """instruction to set the I register to the memory address of the
+        sprite for the value in a given register"""
+        pass;
+
+    def LDbcd(self, reg):
+        """instruction to store the decimal representation of the value in
+        a register in the memory addresses directly after the i register"""
+        val = self.regs[reg];
+        
+        hundreds = val // 100;
+        val -= 100 * hundreds;
+        tens = val // 10;
+        val -= 10 * tens;
+        ones = val;
+
+        self.mem[self.regI] = hundreds;
+        self.mem[self.regI+1] = tens;
+        self.mem[self.regI+2] = ones;
+
+        self.pc += 1;
+
+    def LDmemreg(self, reg):
+        """instruction to store the values in registers 0 - x in memory
+        starting at the value in the I register"""
+        loc = self.regI;
+        for r in range(0, reg+1): #TODO: are we sure this should be inclusive?
+            self.mem[loc] = self.regs[r];
+            loc += 1;
+        self.pc += 1;
+
+    def LDregmem(self, reg):
+        """instruction to load values from memory starting at the address
+        pointed to by the I register into registers 0 to x"""
+        loc = self.regI;
+        for r in range(0, reg+1): #TODO: are we sure this should be inclusive?
+            self.regs[r] = self.mem[loc];
+            loc += 1;
+        self.pc += 1;
+
 
 def main():
     pass;
